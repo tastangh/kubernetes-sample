a5b4eeb0633f32dc561c07f4fb6e513ce1723d32
tastan <mehmet.tastan@tai.com.tr>
2025-07-07 17:38:34 +0300

    first version

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+# backend/app.py
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# Frontend'den gelen istekler için CORS'u etkinleştir
+CORS(app)
+
+@app.route('/api')
+def get_message():
+    return jsonify(message="Merhaba Kubernetes!", version="1.0")
+
+if __name__ == '__main__':
+    # Konteyner içinde 0.0.0.0 üzerinden erişilebilir olmalı
+    app.run(host='0.0.0.0', port=5000)
